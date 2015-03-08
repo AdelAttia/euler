4c6d369c99929fc13dc5b35abfd66320c3ccbfeb
Adel Attia <attia3adel@gmail.com>
2015-03-08 15:53:02 +0100

    First problem

---
+++ 001.py
@@ -0,0 +1,14 @@
+from time import time
+start = time()
+
+s = 0
+for i in xrange(1, 1000):
+    if i % 3 == 0 or i % 5 == 0:
+        s += i
+
+print (s)
+
+end = time()
+total_time = end - start
+
+print ("done in : {} ms".format(total_time))
